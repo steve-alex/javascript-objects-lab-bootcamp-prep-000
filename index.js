dc8326b888facb7be1d3c3c1fc07c7446e67f5e9
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-12 12:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  delete object.key;
+  delete object.key = value;
   return object
 }
