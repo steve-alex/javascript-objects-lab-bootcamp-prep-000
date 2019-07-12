aad747e366c1cbe79e52c20e47b3185e778f1aab
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-12 11:59:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign(recipes, object, {[key]:value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  delete object[key];
+  return object
+}
